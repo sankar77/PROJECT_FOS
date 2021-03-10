79077ae564a8bb90acd5260d54292244cd0bb66e
AbhiniveshP <pabhinivesh@gmail.com>
2021-03-09 17:24:44 -0700

    added local ip address to frontend

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const ip = require('ip');
+const ipAddress = ip.address();
+console.log(ip.address());
+
 const Person = props => (
     <ul className="list-group list-group-flush">
         <li className="list-group-item">{props.person.firstname}</li>
@@ -18,7 +22,7 @@ export default class AboutUs extends Component {
     
 
     componentDidMount() {
-        axios.get('https://main.d8jk9hp5txm1j.amplifyapp.com/aboutus/')
+        axios.get(`http://${ipAddress}:5000/aboutus/`)
           .then(res => {
             console.log(res.data)
             this.setState({ aboutusall: res.data })
