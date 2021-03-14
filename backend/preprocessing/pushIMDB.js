855a2abec24e30024688d2b72fc72fee24f4fe57
AbhiniveshP <pabhinivesh@gmail.com>
2021-03-14 12:47:53 -0600

    data preprocessing setup complete.

---
+++ backend/preprocessing/pushIMDB.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const StreamArray = require( 'stream-json/streamers/StreamArray');
+const fs = require('fs');
+
+const firebase = require('../db');
+const firestore = firebase.firestore();
+
+const jsonStream = StreamArray.withParser();
+
+fs.createReadStream('show_mappings.json').pipe(jsonStream.input);
+
+jsonStream.on('data', async ({key, value}) => {
+    await firestore.collection('imdb_shows').doc().set(value);
+    console.log(key);
+});
+
+jsonStream.on('end', async () => {
+    await console.log('All Done');
+});
