29ff3511b370b6a6256187db53d22c17e9f2737a
TiffanyVPhan <tiffany.phan@colorado.edu>
2021-03-20 13:41:47 -0600

    added app + navbar route to register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import NavBar from './components/NavBar';
 import AboutUs from './components/AboutUs';
+import Register from './components/Register';
+import {AuthProvider} from './contexts/AuthProvider';
 
 function App() {
-  return (
-    <Router>
-      <div className="container">
-        <NavBar />
-        <br/>
-        <Route path="/aboutuspage" exact component={AboutUs} />
-      </div>
-    </Router>
-  );
+    return (
+        // <AuthProvider>
+            <Router>
+                <div className="container">
+                    <NavBar/>
+                    <br/>
+                    <Route path="/aboutuspage" exact component={AboutUs}/>
+                    <Route path="/register" exact component={Register}/>
+                </div>
+            </Router>
+        //</AuthProvider>
+    );
 }
 
 export default App;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,29 +10,32 @@ const NavBar = () => {
     }
 
     return (
-        <div className= 'container'>
+        <div className='container'>
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto col-3">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/aboutuspage">About Us</a>
-                    </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/aboutuspage">About Us</a>
+                        </li>
                     </ul>
                     <form className="form-inline my-2 my-lg-0 col-6">
                         <label htmlFor="searchValue"></label>
-                    <input className="form-control mr-sm-2 col-8" type="search" placeholder="Search Movies/TV Shows" aria-label="Search" id="searchValue"/>
-                    <button className="btn btn-outline-primary my-2 my-sm-0" type="submit" onClick={onClick}>Search</button>
+                        <input className="form-control mr-sm-2 col-8" type="search" placeholder="Search Movies/TV Shows"
+                               aria-label="Search" id="searchValue"/>
+                        <button className="btn btn-outline-primary my-2 my-sm-0" type="submit"
+                                onClick={onClick}>Search
+                        </button>
                     </form>
                     <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Login</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/">Register</a>
-                    </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/register">Register</a>
+                        </li>
                     </ul>
                 </div>
             </nav>
