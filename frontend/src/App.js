d28dc328b42daad1130ec840270a690e8abf43e6
AbhiniveshP <pabhinivesh@gmail.com>
2021-03-22 14:59:17 -0600

    avoiding merge conflicts in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,19 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import {Container, Row, Col} from "react-bootstrap";
 
 import NavBar from './components/NavBar';
 import AboutUs from './components/AboutUs';
-import MovieCard from './components/MovieCard';
-import TVShowCard from './components/TVShowCard';
 
 function App() {
   return (
-    <>
-      <Router>
+    <Router>
+      <div className="container">
         <NavBar />
+        <br/>
         <Route path="/aboutuspage" exact component={AboutUs} />
-      </Router>
-
-      <Container className="justify-content-around" style={{display:'flex'}}>
-        <Row>
-          <Col><MovieCard id="862"/></Col>
-          <Col><TVShowCard id="1402"/></Col>
-          <Col><MovieCard id="1402"/></Col>
-          <Col><TVShowCard id="1401"/></Col>
-        </Row>
-      </Container>
-    </>
+      </div>
+    </Router>
   );
 }
 
