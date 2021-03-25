5400230d8bbffcb0b5d23183b7dd11457183d108
sankar77 <nsnkrnryn@gmail.com>
2021-03-25 10:55:30 -0600

    Merged main into manage-homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,10 @@ import ForgotPassword from './components/ForgotPassword';
 import Profile from './components/Profile'
 import Account from "./components/Account";
 import UpdateAccount from "./components/UpdateAccount";
+import MoviesList from './components/MoviesList';
+import Search from './components/Search';
 import { AuthProvider } from './contexts/AuthProvider';
+import TodayTV from './components/TodayTV';
 
 function App() {
     return (
@@ -19,7 +22,6 @@ function App() {
                 <div className="container">
                     <NavBar/>
                     <br/>
-                    <Route path="/" exact component={() => <></>}/>
                     <Route path="/aboutuspage" exact component={AboutUs}/>
                     <Route path="/signup" exact component={SignUp}/>
                     <Route path="/login" exact component={Login}/>
@@ -27,10 +29,13 @@ function App() {
                     <Route path="/profile" exact component={Profile}/>
                     <Route path="/account" exact component={Account}/>
                     <Route path="/update-account" exact component={UpdateAccount}/>
+                    <Route path = "/" exact component = {MoviesList}/>
+                    <Route path = "/search" exact component = {Search}/>
+                    <Route path = "/today" exact component = {TodayTV}/>
                 </div>
             </Router>
         </AuthProvider>
     );
-}
+    }
 
 export default App;
