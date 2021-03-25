9d9f8074a1db24e5b6ce538f57583a2140cd1362
sankar77 <nsnkrnryn@gmail.com>
2021-03-25 17:59:28 -0600

    Added base route for backend

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-var https = require('https');
+// var https = require('https');
 var http = require('http');
 var fs = require('fs');
 const config = require('./config');
 const aboutUsRoutes = require('./routes/aboutus');
-var os = require('os');
-const homedir = os.homedir();
+// var os = require('os');
+// const homedir = os.homedir();
 
-var key = fs.readFileSync(homedir + '/certs/server.key');
-var cert = fs.readFileSync(homedir + '/certs/server.cert');
+// var key = fs.readFileSync(homedir + '/certs/server.key');
+// var cert = fs.readFileSync(homedir + '/certs/server.cert');
 
-var options = {
-  key: key,
-  cert: cert
-};
+// var options = {
+//   key: key,
+//   cert: cert
+// };
 
 const app = express();
 
@@ -24,17 +24,20 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/aboutus', aboutUsRoutes.routes);
+// app.use('/aboutus', aboutUsRoutes.routes);
 
-// app.listen(config.port, () => {
-//     console.log(`Server is running on port: ${config.port}`);
-// });
-// http.createServer(app).listen(80);
+app.listen(config.port, () => {
+    console.log(`Server is running on port: ${config.port}`);
+});
+app.get('/',function(req,res){
+  res.send("Hello World");
+})
+
 // https.createServer(options, app).listen(443);
 
-const httpsAgent = new https.Agent({ rejectUnauthorized: false });
-var server = https.createServer(options, app);
+// const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+// var server = https.createServer(options, app);
 
-server.listen(config.port, () => {
-  console.log("server starting on port : " + config.port)
-});
+// server.listen(config.port, () => {
+//   console.log("server starting on port : " + config.port)
+// });
