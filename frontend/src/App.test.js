3ac4a7ecf0e27cafc64ac028f0feb27e81fe497b
neel010427 <neelotpalkaturi@gmail.com>
2021-04-05 09:18:40 -0600

    added search bar and button acceptance tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -19,18 +19,40 @@ describe('User should see nav bar', () => {
     })
     expect(result).toEqual('Home')
 
-    })
-
-    test('All navbar tabs show up', async () => {
-      await page.goto(`${appUrlBase}/`)
-      await page.waitForSelector('.container')
-      const books = await page.evaluate(() => {
-        return [...document.querySelectorAll('.nav-item .nav-link')].map(el => el.innerText)
-      })
-  
-      expect(books.length).toEqual(4)
   })
 
+  test('All navbar tabs show up', async () => {
+    await page.goto(`${appUrlBase}/`)
+    await page.waitForSelector('.container')
+    const books = await page.evaluate(() => {
+      return [...document.querySelectorAll('.nav-item .nav-link')].map(el => el.innerText)
+    })
+
+    expect(books.length).toEqual(4)
+  })
+
+  test('Search', async () => {
+    jest.setTimeout(10)
+    await page.goto(`${appUrlBase}/`)
+    await page.waitForSelector('Button')
+    const result = await page.evaluate(() => {
+      return document.querySelector('Button').innerText
+    })
+    expect(result).toEqual('Search')
+
+  })
+
+  test('Search bar', async () => {
+    jest.setTimeout(15000)
+    await page.goto(`${appUrlBase}/`)
+    await page.waitForSelector('input')
+    const result = await page.evaluate(() => {
+      return document.querySelector('input').placeholder
+    })
+    expect(result).toEqual('Search Movies/TV Shows')
+
+  })
+  
 })
 
 afterAll(() => {
