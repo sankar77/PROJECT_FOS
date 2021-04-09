a908014d99737e2d9d0f407144e9ec6b7f2643ac
TiffanyVPhan <tiffany.phan@colorado.edu>
2021-04-09 17:56:22 -0600

    add working message queue to scheduler

--- collector/controllers/collector.js
+++ collector/controllers/collector.js
@@ -1,91 +1,81 @@
-const { response } = require('express');
+const {response} = require('express');
 const fetch = require('node-fetch');
 const axios = require('axios');
 const firebase = require('../db');
 const collector = require('../models/collector');
 const firestore = firebase.firestore();
+const {pollFromQueue, pushToQueue, queueURL} = require('../queues/main');
 
-const apiKey="42d845ec0caf10ecc9f34f1648197aee";
+const apiKey = "42d845ec0caf10ecc9f34f1648197aee";
 const videoBase = "https://www.youtube.com/watch?v=";
-
-const State = {
-    movieList: [],
-    tvshowList: []    
-}
 
 // get the list of now playing ids for movies
 const movieDetails = () => {
-    
-    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=5`;  
-    return new Promise( (resolve, reject) => {
+
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=5`;
+    return new Promise((resolve, reject) => {
 
         fetch(url)
-        .then(response => response.json())
-        .then( (data) => {
+            .then(response => response.json())
+            .then((data) => {
 
-            
-            var i = 0;
-            var fullmovieList=[]
-            for(;i<data.results.length;i++){
-                fullmovieList.push(data.results[i].id)
+
+                var i = 0;
+                var fullmovieList = []
+                for (; i < data.results.length; i++) {
+                    fullmovieList.push(data.results[i].id)
 
                 }
 
                 resolve({
-                    'movies': fullmovieList
-                },
-                (error) => {
-                    reject(error);
-                }
-            )
-        })
-            
+                        'movies': fullmovieList
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                )
+            })
+
     })
 }
 
 // get the list of now playing ids for tvshows
 
 const tvshowDetails = () => {
-    
-    const url = `https://api.themoviedb.org/3/tv/airing_today?api_key=${apiKey}&language=en-US&page=1`;  
-    return new Promise( (resolve, reject) => {
+
+    const url = `https://api.themoviedb.org/3/tv/airing_today?api_key=${apiKey}&language=en-US&page=1`;
+    return new Promise((resolve, reject) => {
 
         fetch(url)
-        .then(response => response.json())
-        .then( (data) => {
+            .then(response => response.json())
+            .then((data) => {
 
-            
-            var i = 0;
-            var tvList=[]
-            for(;i<data.results.length;i++){
-                tvList.push(data.results[i].id)
+
+                var i = 0;
+                var tvList = []
+                for (; i < data.results.length; i++) {
+                    tvList.push(data.results[i].id)
 
                 }
 
                 resolve({
-                    'tvshows': tvList
-                },
-                (error) => {
-                    reject(error);
-                }
-            )
-        })
-            
+                        'tvshows': tvList
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                )
+            })
+
     })
 }
 
 
-
-
-
 const getnowPlaying = async (req, res) => {
-    var moviedetails = await movieDetails();
-    State.movieList = moviedetails.movies;
-    var tvshowdetails = await tvshowDetails();
-    State.tvshowList = tvshowdetails.tvshows;
-    try
-    {
-        let flag=true;
+    let State = await pollFromQueue(queueURL);
+
+    try {
+        let flag = true;
 
         const moviesTvshows = await firestore.collection('collector');
         const data = await moviesTvshows.get();
@@ -93,16 +83,25 @@ const getnowPlaying = async (req, res) => {
         if (!data.empty) {
             firestore.collection('collector').doc('new_doc').delete();
         }
-        
-        firestore.collection('collector').doc('new_doc').set(JSON.parse( JSON.stringify(State)));
-    }
-    catch (error) {
-        
-        console.log("error is",error.message);
+
+        firestore.collection('collector').doc('new_doc').set(JSON.parse(JSON.stringify(State)));
+    } catch (error) {
+
+        console.log("error is", error.message);
     }
 
-    
+    State = {
+        movieList: [],
+        tvshowList: []
+    }
+    var moviedetails = await movieDetails();
+    State.movieList = moviedetails.movies;
+    var tvshowdetails = await tvshowDetails();
+    State.tvshowList = tvshowdetails.tvshows;
+
+    pushToQueue(queueURL, State);
 }
+
 
 module.exports = {
     getnowPlaying
--- collector/queues/main.js
+++ collector/queues/main.js
@@ -8,48 +8,48 @@ AWS.config.update({region: 'us-east-2'});
 
 mockObject = {
     movieList: [
-    580532,
-    580175,
-    645856,
-    627103,
-    806067,
-    498248,
-    463257,
-    634521,
-    631058,
-    799878,
-    665779,
-    549294,
-    587562,
-    654754,
-    861,
-    811828,
-    575776,
-    566076,
-    638045,
-    717021
+        580532,
+        580175,
+        645856,
+        627103,
+        806067,
+        498248,
+        463257,
+        634521,
+        631058,
+        799878,
+        665779,
+        549294,
+        587562,
+        654754,
+        861,
+        811828,
+        575776,
+        566076,
+        638045,
+        717021
     ],
     tvshowList: [
-    88396,
-    1416,
-    95557,
-    79460,
-    67133,
-    76773,
-    2734,
-    99121,
-    102966,
-    71728,
-    117422,
-    60572,
-    80563,
-    82822,
-    104699,
-    99618,
-    32692,
-    1220,
-    87917,
-    65988
+        88396,
+        1416,
+        95557,
+        79460,
+        67133,
+        76773,
+        2734,
+        99121,
+        102966,
+        71728,
+        117422,
+        60572,
+        80563,
+        82822,
+        104699,
+        99618,
+        32692,
+        1220,
+        87917,
+        65988
     ]
 }
 
@@ -58,15 +58,15 @@ var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 var queueURL = "https://sqs.us-east-2.amazonaws.com/274670721002/movies-app-queue";
 
 
-function pushToQueue(queueURL, messageJSON){
-    var params = {  
+function pushToQueue(queueURL, messageJSON) {
+    var params = {
         MessageBody: JSON.stringify(messageJSON),
         QueueUrl: queueURL
-      };
+    };
 
-      sqs.sendMessage(params, function(err, data) {
+    sqs.sendMessage(params, function (err, data) {
         if (err) {
-          console.log("Error", err);
+            console.log("Error", err);
         } else {
             let date = ("0" + date_ob.getDate()).slice(-2);
             let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
@@ -74,51 +74,57 @@ function pushToQueue(queueURL, messageJSON){
             let hours = date_ob.getHours();
             let minutes = date_ob.getMinutes();
             let seconds = date_ob.getSeconds();
-          console.log(`Pushing data id: ${data.MessageId} to Queue at ${year}-${month}-${date} ${hours}:${minutes}:${seconds}`);        }
-      });
-}
-
-function pollFromQueue(queueURL) {
-    var params = {
-        AttributeNames: [
-           "SentTimestamp"
-        ],
-        MaxNumberOfMessages: 10,
-        MessageAttributeNames: [
-           "All"
-        ],
-        QueueUrl: queueURL,
-        VisibilityTimeout: 20,
-        WaitTimeSeconds: 0
-       };
-
-       sqs.receiveMessage(params, function(err, data) {
-        if (err) {
-          console.log("Receive Error", err);
-        } else if (data.Messages) {
-        //   console.log(JSON.parse(data.Messages[0].Body));
-            let date = ("0" + date_ob.getDate()).slice(-2);
-            let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-            let year = date_ob.getFullYear();
-            let hours = date_ob.getHours();
-            let minutes = date_ob.getMinutes();
-            let seconds = date_ob.getSeconds();
-          console.log(`Data Fetched from Queue at ${year}-${month}-${date} ${hours}:${minutes}:${seconds}`)
-          var deleteParams = {
-            QueueUrl: queueURL,
-            ReceiptHandle: data.Messages[0].ReceiptHandle
-          };
-          sqs.deleteMessage(deleteParams, function(err, data) {
-            if (err) {
-              console.log("Delete Error", err);
-            } else {
-              console.log("Message Deleted", data);
-            }
-          });
+            console.log(`Pushing data id: ${data.MessageId} to Queue at ${year}-${month}-${date} ${hours}:${minutes}:${seconds}`);
         }
-      });
+    });
 }
 
-pollFromQueue(queueURL);
-pushToQueue(queueURL, mockObject);
-// setTimeout(pollFromQueue, 3000, queueURL);
+async function pollFromQueue(queueURL) {
+    return new Promise(res => {
+        var params = {
+            AttributeNames: [
+                "SentTimestamp"
+            ],
+            MaxNumberOfMessages: 10,
+            MessageAttributeNames: [
+                "All"
+            ],
+            QueueUrl: queueURL,
+            VisibilityTimeout: 20,
+            WaitTimeSeconds: 0
+        };
+
+        sqs.receiveMessage(params, function (err, data) {
+            if (err) {
+                console.log("Receive Error", err);
+            } else if (data.Messages) {
+                const State = JSON.parse(data.Messages[0].Body);
+                let date = ("0" + date_ob.getDate()).slice(-2);
+                let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+                let year = date_ob.getFullYear();
+                let hours = date_ob.getHours();
+                let minutes = date_ob.getMinutes();
+                let seconds = date_ob.getSeconds();
+                console.log(`Data Fetched from Queue at ${year}-${month}-${date} ${hours}:${minutes}:${seconds}`)
+                var deleteParams = {
+                    QueueUrl: queueURL,
+                    ReceiptHandle: data.Messages[0].ReceiptHandle
+                };
+                sqs.deleteMessage(deleteParams, function (err, data) {
+                    if (err) {
+                        console.log("Delete Error", err);
+                    } else {
+                        console.log("Message Deleted", data);
+                        res(State);
+                    }
+                });
+            }
+        });
+    });
+}
+
+module.exports = {
+    pollFromQueue,
+    pushToQueue,
+    queueURL
+}
