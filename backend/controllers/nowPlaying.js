59ee1d95dc76af982d4b2a3d4b9c4efe78dc0dae
sankar77 <nsnkrnryn@gmail.com>
2021-04-16 11:16:12 -0600

    Added back newrelic dependency

--- backend/controllers/nowPlaying.js
+++ backend/controllers/nowPlaying.js
@@ -3,8 +3,6 @@ const fetch = require('node-fetch');
 const axios = require('axios');
 const firebase = require('../db');
 const firestore = firebase.firestore();
-const {pollFromQueue, pushToQueue, queueURL} = require('../queues/main');
-
 
 const apiKey="42d845ec0caf10ecc9f34f1648197aee";
 const videoBase = "https://www.youtube.com/watch?v=";
@@ -58,36 +56,17 @@ const State = {
 
 
 const getnowPlaying = async (req, res) => {
-    let State = await pollFromQueue(queueURL);
 
-    try {
-        let flag = true;
-
-        const moviesTvshows = await firestore.collection('collector');
-        const data = await moviesTvshows.get();
-
-        if (!data.empty) {
-            firestore.collection('collector').doc('new_doc').delete();
-        }
-
-        firestore.collection('collector').doc('new_doc').set(JSON.parse(JSON.stringify(State)));
-    } catch (error) {
-
-        console.log("error is", error.message);
-    }
-
-    State = {
-        movieList: [],
-        tvshowList: []
-    }
     var moviedetails = await movieDetails();
+
     State.movieList = moviedetails.movies;
+    // console.log("movies is",State.movieList);
     var tvshowdetails = await tvshowDetails();
     State.tvshowList = tvshowdetails.tvshows;
+    // console.log("tv show",State.tvshowList);
 
-    pushToQueue(queueURL, State);
-    
-    res.send(State);
+    res.send(State)
+
 }
 
 
